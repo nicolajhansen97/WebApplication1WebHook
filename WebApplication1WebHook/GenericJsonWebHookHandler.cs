2e42a3843b41b2ba6107fe93802a23c6d364cdcd
nicolajhansen97 <nicolajhansen97@live.dk>
2022-05-10 13:11:37 +0200

    Paart 1

--- WebApplication1WebHook/GenericJsonWebHookHandler.cs
+++ WebApplication1WebHook/GenericJsonWebHookHandler.cs
@@ -19,7 +19,11 @@ namespace WebApplication1WebHook
 
         public async override Task ExecuteAsync(string receiver, WebHookHandlerContext context)
         {
-            string secret = "PASSWORD";
+            string secret = "";
+            string Username = "nicol";
+            string Domain = "workgroup";
+            string CreateCustomerUri = @"http://laptop-jos7ofn3:7048/BC170/ODataV4/CreateCustomer_CreateCustomer?company=CRONUS%20UK%20Ltd.";
+            string CreateOrderUri = @"http://laptop-jos7ofn3:7048/BC170/ODataV4/CreateOrder_CreateOrder?company=CRONUS%20UK%20Ltd.";
 
 
 
@@ -53,8 +57,7 @@ namespace WebApplication1WebHook
 
 
 
-
-            System.Diagnostics.Debug.WriteLine("Called1-----------------------");
+        System.Diagnostics.Debug.WriteLine("Called1-----------------------");
 
 
             // Get JSON from WebHook
@@ -67,7 +70,7 @@ namespace WebApplication1WebHook
 
                 string whichType = context.Id;
 
-                if (whichType.Equals("z"))
+                if (whichType.Equals(customerWeebhook))
                 {
 
                     try
@@ -79,10 +82,12 @@ namespace WebApplication1WebHook
                         string customerEmail = data.GetValue("email").ToString();
                         string customerUsername = data.GetValue("username").ToString();
 
-                        Uri adress = new Uri(@"http://laptop-jos7ofn3:7048/BC170/ODataV4/CreateCustomer_CreateCustomer?company=CRONUS%20UK%20Ltd.");
+
+
+                        Uri adress = new Uri(CreateCustomerUri);
 
                         var credentialsCache = new CredentialCache();
-                        credentialsCache.Add(adress, "NTLM", new NetworkCredential("nicol", secret, "workgroup"));//Negotiate
+                        credentialsCache.Add(adress, "NTLM", new NetworkCredential(Username, secret, Domain));//Negotiate
                         var handler = new HttpClientHandler() { Credentials = credentialsCache, PreAuthenticate = true };//, PreAuthenticate = true
 
 
@@ -112,22 +117,65 @@ namespace WebApplication1WebHook
                     }
 
                 }
-                else if(whichType.Equals("c"))
+                else if(whichType.Equals(newOrderWebhook))
                 {
+                    //int customerID = int.Parse(data.GetValue("customerID").ToString());
                     int customerID = int.Parse(data.GetValue("customer_id").ToString());
-          //          string firstName = data.GetValue("first_name").ToString();
-            //        string lastName = data.GetValue("last_name").ToString();
-              //      string customerEmail = data.GetValue("email").ToString();
-                //    string adress = data.GetValue("adress_1").ToString();
-                  //  string city = data.GetValue("city").ToString();
+                    var billing = JObject.Parse(data.GetValue("billing").ToString());
+                    string firstName = billing.GetValue("first_name").ToString();
+                    string lastName = billing.GetValue("last_name").ToString();
+                    string customerEmail = billing.GetValue("email").ToString();
+                    string adress = billing.GetValue("address_1").ToString();
+                    string city = billing.GetValue("city").ToString();
+                    string postCode = billing.GetValue("postcode").ToString();
+                    string country = billing.GetValue("country").ToString();
+                    string phone = billing.GetValue("phone").ToString();
+                    int lineSize = data.GetValue("line_items").Count();
+                    string[] name = new string[lineSize];
+                    string[] productId = new string[lineSize];
+                    string[] quantity = new string[lineSize];
+                    string[] subtotal = new string[lineSize];
+
+                    for (int i = 0; i < lineSize; i++)
+                    {
+                        var lineItems = JObject.Parse(data.GetValue("line_items")[i].ToString());
+                        name[i] = lineItems.GetValue("name").ToString();
+                        productId[i] = lineItems.GetValue("product_id").ToString();
+                        quantity[i] = lineItems.GetValue("quantity").ToString();
+                        subtotal[i] = lineItems.GetValue("subtotal").ToString();
+                        //var lineItems = JObject.Parse(data.GetValue("line_items")[i].ToString());
+
+                    }
+
+                    //var lineItems = JObject.Parse(data.GetValue("line_items")[i].ToString());
+
+                    //string first = lineItems.GetValue("name")[0].ToString();
+
+
+                    //postCode = postCode, country = country, phone = phone}); ;
+
+                    /*
+                    for (int i = 0; i < billing.Count(); i++)
+                    {
+                        billingArray[i] = billing;
+                    }
+                    */
+
+                    //          string firstName = data.GetValue("first_name").ToString();
+                    //        string lastName = data.GetValue("last_name").ToString();
+                    //      string customerEmail = data.GetValue("email").ToString();
+                    //    string adress = data.GetValue("adress_1").ToString();
+                    //  string city = data.GetValue("city").ToString();
                     //string postCode = data.GetValue("postcode").ToString();
                     //string country = data.GetValue("country").ToString();
                     //string phone = data.GetValue("phone").ToString();
 
-                    Uri adress2 = new Uri(@"http://laptop-jos7ofn3:7048/BC170/ODataV4/CreateCustomer_CreateCustomer?company=CRONUS%20UK%20Ltd.");
+                    //Uri adress2 = new Uri(@"http://laptop-jos7ofn3:7048/BC170/ODataV4/CreateOrder_CreateOrder?company=CRONUS%20UK%20Ltd.");
+                    Uri adress2 = new Uri(CreateOrderUri);
 
                     var credentialsCache = new CredentialCache();
-                    credentialsCache.Add(adress2, "NTLM", new NetworkCredential("nicol", secret, "workgroup"));//Negotiate
+                    //credentialsCache.Add(adress2, "NTLM", new NetworkCredential("nicol", secret, "workgroup"));//Negotiate
+                    credentialsCache.Add(adress2, "NTLM", new NetworkCredential(Username, secret, Domain));//Negotiate
                     var handler = new HttpClientHandler() { Credentials = credentialsCache, PreAuthenticate = true };//, PreAuthenticate = true
 
 
@@ -137,15 +185,18 @@ namespace WebApplication1WebHook
                         //httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
                         //var response = await httpClient.PostAsJsonAsync(adress, "{'id':1}");
                         //var response = await httpClient.GetAsync(adress);
-                        var response = await httpClient.PostAsJsonAsync(adress2, new Order { customerID = customerID }); ; //firstName = firstName, lastName = lastName, customerEmail = customerEmail, adress = adress, city = city , postCode = postCode, country = country, phone = phone}); ;
+                        //var response = await httpClient.PostAsJsonAsync(adress2, new Order { customerID = customerID }); //,firstName = firstName , lastName = lastName, customerEmail = customerEmail, adress = adress, city = city , postCode = postCode, country = country, phone = phone}); ;
+                        var dataString = data.ToString();
+                        var response = await httpClient.PostAsJsonAsync(adress2, new { obj = dataString }); //,firstName = firstName , lastName = lastName, customerEmail = customerEmail, adress = adress, city = city , postCode = postCode, country = country, phone = phone}); ;
 
                         var result = await response.Content.ReadAsStringAsync();
 
                         if (response.IsSuccessStatusCode)
-                            System.Diagnostics.Debug.WriteLine("CreateCustomer call result: " + result);
+                            System.Diagnostics.Debug.WriteLine("CreateOrder call result: " + result);
                         else
-                            System.Diagnostics.Debug.WriteLine("CreateCustomer call error: " + response.StatusCode.ToString());
+                            System.Diagnostics.Debug.WriteLine("CreateOrder call error: " + response.StatusCode.ToString());
                     }
+
 
 
 
@@ -172,7 +223,7 @@ namespace WebApplication1WebHook
 
     }
 
-    class Order
+    class Order2
     {
         public int customerID { get; set; }
         public string firstName { get; set; }
@@ -184,5 +235,11 @@ namespace WebApplication1WebHook
         public string country { get; set; }
         public string phone { get; set; }
 
+    }
+
+    class Order
+    {
+        public int customerID { get; set; }
+        public string firstName { get; set; }
     }
 }
